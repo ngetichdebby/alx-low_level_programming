9ab9c860e2839bd6093ae50fc185646c9ae324fd
ngetichdebby <ngetichdebby1@gmail.com>
2023-10-05 13:02:10 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
 /**
- * main - A program that will print using printf function
- * Return: 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
-}
+ * main -A program that print a line using 
+ *
