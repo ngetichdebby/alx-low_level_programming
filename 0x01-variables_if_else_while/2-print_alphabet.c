02ba589e4119dd07d1bc3e1615f9b419a90fa988
ngetichdebby <ngetichdebby1@gmail.com>
2023-10-06 13:19:22 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z'0) {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
